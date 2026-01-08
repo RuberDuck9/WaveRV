12415acd0d09bd06c4df559bfddde727e4e38ab3
RuberDuck9 <lukehoop0722@gmail.com>
2026-01-07 19:57:38 -0500

    began moving modules to their own files

---
+++ modules/memory.v
@@ -0,0 +1,46 @@
+// **************************************************
+// Full Dual-Port Memory (12 bit block)
+// **************************************************
+
+module memory (
+	input clk_a,
+    input memory_write_enable_a,
+    input [31:0] memory_access_address_a,
+    input [31:0] memory_write_data_a,
+	output reg [31:0] memory_read_data_a,
+    input clk_b,
+    input memory_write_enable_b,
+    input [31:0] memory_access_address_b,
+    input [31:0] memory_write_data_b,
+	output reg [31:0] memory_read_data_b
+);
+
+// -------------- Memory Block --------------
+
+reg [31:0] memory[0:4095];
+
+// ----------------- Port A -----------------
+
+always @(posedge clk_a) begin
+    memory_read_data_a <= memory[memory_access_address_a];
+
+    if (memory_write_enable_a) begin
+        memory_read_data_a <= memory_write_data_a; // Write-before-read explicit implementation
+        memory[memory_access_address_a] <= memory_write_data_a;
+    end
+
+end
+
+// ----------------- Port B -----------------
+
+always @(posedge clk_b) begin
+    memory_read_data_b <= memory[memory_access_address_b];
+
+    if (memory_write_enable_b) begin
+        memory_read_data_b <= memory_write_data_b; // Write-before-read explicit implementation
+        memory[memory_access_address_b] <= memory_write_data_b;
+    end
+    
+end
+
+endmodule
---
+++ modules/register_file.v
@@ -0,0 +1,41 @@
+// **************************************************
+// 32 Wide (5 bit) 32 Bit Write-before-read Register File
+// **************************************************
+
+module register_file(
+	input wire clk,
+	input wire register_write_enable,
+	input wire [4:0] register_write_address,
+	input wire [4:0] register_read_address_a,
+	input wire [4:0] register_read_address_b,
+	input wire [31:0] register_write_data,
+	output reg [31:0] register_read_data_a,
+	output reg [31:0] register_read_data_b
+);
+
+// -------------- Register File --------------
+
+reg [31:0] register_file [0:31];
+
+// ---------------- W/R Logic ----------------
+
+always @(posedge clk) begin
+    register_read_data_a <= register_file[register_read_address_a];
+    register_read_data_b <= register_file[register_read_address_b];
+
+	if (register_write_enable) begin
+		register_file[register_write_address] <= register_write_data;
+
+        if (register_write_address == register_read_address_a) begin
+            register_read_data_a <= register_write_data; // Write-before-read explicit implementation
+        end
+
+        if (register_write_address == register_read_address_b) begin
+            register_read_data_b <= register_write_data; // Write-before-read explicit implementation
+        end  
+
+	end 
+    
+end
+
+endmodule
